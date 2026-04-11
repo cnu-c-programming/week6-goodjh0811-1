a52512e3bb06e415f84d06fca4611a370e316931
박지현202500924 <goodjh0811@gmail.com>
2026-04-11 23:52:43 +0900

    Update 3-3.c

--- code/3-3.c
+++ code/3-3.c
@@ -11,6 +11,7 @@ int main() {
     printf("%p\n", (void*)p2);
     printf("%p\n", (void*)p3);
     printf("\n");
-    printf("%lu\n", (unsigned long long)(p1 + 1) - (unsigned long long(p1));
-    printf("%lu\n", (unsigned long long)(p2 + 1) - (unsigned long long(p2));
-    printf("%lu\n", (unsigned long long)(p3 + 1) - (unsigned long long(p3));
+    printf("%llu\n", (unsigned long long)(p1 + 1) - (unsigned long long)(p1));
+    printf("%llu\n", (unsigned long long)(p2 + 1) - (unsigned long long)(p2));
+    printf("%llu\n", (unsigned long long)(p3 + 1) - (unsigned long long)(p3));
+}
