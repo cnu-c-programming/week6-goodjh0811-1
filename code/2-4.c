1b894957556989d5b9d37209b7698680a02778f2
박지현202500924 <goodjh0811@gmail.com>
2026-04-11 23:49:56 +0900

    Update 2-4.c

--- code/2-4.c
+++ code/2-4.c
@@ -2,8 +2,8 @@
 
 void print_2d_arr(int* rows[], int row_size, int col_size) {
     for(int i = 0; i < row_size; i++) {
-        for (int j = 0; j < col_size; j++);
-            printf("%d ", *(*(row + i) + j));
+        for (int j = 0; j < col_size; j++) {
+            printf("%d ", *(*(rows + i) + j));
         }
         printf("\n");
     }
