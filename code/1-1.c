ab92421eb159177679b394fa58f94f8e76eacb0b
박지현202500924 <goodjh0811@gmail.com>
2026-04-11 23:28:42 +0900

    Update 1-1.c

--- code/1-1.c
+++ code/1-1.c
@@ -6,14 +6,14 @@ int main() {
   int a = 10, b = 20, c = 30, d = 40, e = 50;
   int * pointer_array[5] = {&a, &b, &c, &d, &e};
 
-  printf("sizeof(array_pointer); %zu\n", sizeof(array_pointer));
-  printf("sizeof(array_pointer); %zu\n", sizeof(pointer_array));
+  printf("sizeof(array_pointer): %zu\n", sizeof(array_pointer));
+  printf("sizeof(array_array): %zu\n", sizeof(pointer_array));
   printf("\n");
   for (int i = 0; i < 4; i++) {
       printf("%p\n", (void*)(array_pointer + i));
   }
   printf("\n");
   for (int i = 0; i < 4; i++) {
-      printf("%o\n", (void*)(pointer_array + i));
+      printf("%p\n", (void*)(pointer_array + i));
   }
 }
